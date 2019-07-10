52e45cc1af326b65f8cc412215de2816f88de459
Shelley Arnold <shelle.kay@gmail.com>
2019-07-10 10:15:49 -0500

    constructors

--- Car.cs
+++ Car.cs
@@ -2,33 +2,48 @@ using System;
 
 namespace randalls_car_lot
 {
-  class Car
-  {
-    public int Year { get; set; }
-
-    public string Model { get; set; }
-
-    public string Make { get; set; }
-
-    public double Price { get; set; }
-
-    //method -----void means you return nothing
-    public void Drive()
+    class Car
     {
-      if (Make == null)
-      {
-        Console.WriteLine($"Sorry Dog, no make");
-      }
-      else
-      {
-        Console.WriteLine($"I'm drivin' my {Make} here!!");
-      }
-    }
+        //this is referred to as a field, does not have a get/setter 
+        private int _miles = 0;
 
-    public void Drive(int miles)
-    {
-      Console.WriteLine($"{Make} is driving for {miles} miles");
-    }
+        //these are referred to as propterties
+        public int Year { get; set; }
 
-  }
+        public string Model { get; set; }
+
+        public string Make { get; set; }
+
+        public double Price { get; set; }
+
+        //method -----void means you return nothing
+        public void Drive()
+        {
+            if (Make == null)
+            {
+                Console.WriteLine($"Sorry Dog, no make");
+            }
+            else
+            {
+                Console.WriteLine($"I'm drivin' my {Make} here!!");
+            }
+        }
+
+        //constructor above methods
+        public Car(int startingMiles)
+        {
+            _miles = startingMiles;
+            Console.WriteLine("A new car is being instantiated");
+        }
+
+        public Car() { }
+
+        public void Drive(int miles)
+        {
+            _miles += miles;
+            Console.WriteLine($"{Make} is driving for {miles} miles");
+            Console.WriteLine($"The odometer on the {Make} {Model} now reads {_miles}");
+        }
+
+    }
 }
--- Program.cs
+++ Program.cs
@@ -3,64 +3,35 @@ using System.Collections.Generic;
 
 namespace randalls_car_lot
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      // var carsOnLot = new Dictionary<string, Dictionary<string, string>>();
+        static void Main(string[] args)
+        {
+            //Option 1
+            var ford2 = new Car();
+            ford2.Year = 2017;
+            ford2.Model = "Fiesta";
+            ford2.Make = "Ford";
+            ford2.Price = 32500.65;
 
-      // var vehicle = new Dictionary<string, string>() {
-      // ("year", "2008"),
-      // ("model", "Damfresh"),
-      // ("make", "Biotraxquote"),
-      // ("color", "sky magenta")
-      // };
+            //Option 2
+            var tesla2 = new Car()
+            {
+                Year = 2017,
+                Model = "x",
+                Make = "Tesla",
+                Price = 80000.42,
+            };
 
+            //Add cars to the garage
+            var garage = new List<Car>() { ford2, tesla2 };
 
-      // carsOnLot.Add(vehicle, );
-
-      // Dictionary<string, string> salesAgent = new Dictionary<string, string>();
-      // new Dictionary<string, string>();
-
-      // salesAgent.Add("mobile", "2008");
-      // salesAgent.Add("lastName", "Botsford");
-      // salesAgent.Add("firstName", "Shaina");
-
-      // carsOnLot.Add(salesAgent);
-
-      // Dictionary<string, string> credit = new Dictionary<string, string>();
-      // new Dictionary<string, string>();
-
-      // credit.Add("creditProvider", "J.P.Morgan Chase & Co");
-      // credit.Add("account", "601109582720302");
-
-      // carsOnLot.Add(salesAgent);
-
-      //Option 1
-      var ford2 = new Car();
-      ford2.Year = 2017;
-      ford2.Model = "Fiesta";
-      ford2.Make = "Ford";
-      ford2.Price = 32500.65;
-
-      //Option 2
-      var tesla2 = new Car()
-      {
-        Year = 2017,
-        Model = "x",
-        Make = "Tesla",
-        Price = 80000.42,
-      };
-
-      //Add cars to the garage
-      var garage = new List<Car>() { ford2, tesla2 };
-
-      tesla2.Drive();
-      tesla2.Drive(1000);
+            tesla2.Drive();
+            tesla2.Drive(1000);
 
 
-      // foreach (List<Car> automobile in garage)
-      //   Console.WriteLine($"{automobile}");
+            foreach (Car automobile in garage)
+                automobile.Drive();
+        }
     }
-  }
 }
